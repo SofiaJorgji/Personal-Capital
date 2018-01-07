7e49c18bd76b329d82e2fb1e25604569a6c9d4b6
Sofia Jorgji <jorgjis@tcd.ie>
2018-01-07 15:31:12 -0800

    Create data folder to store data used for the tests

---
+++ test/data/data.js
@@ -0,0 +1,10 @@
+'use strict';
+
+module.exports = {
+	user: {
+		'email': 'stranger@gmail.com',
+		'password': 'FHKHGF8U2',
+		'phoneNumber': '6504678634',
+	},
+	baseUrl: 'https://www.personalcapital.com/',
+};
